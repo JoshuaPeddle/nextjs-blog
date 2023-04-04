9ee9068df3ead585b7cb691fa50ef131af55c3f0
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-04-04 12:55:38 -0230

    Fetch post in real time from DB

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,40 +2,52 @@ import Layout from '../../components/layout';
 import Head from 'next/head';
 import Date from '../../components/date';
 
-import { getAllPostIds, getPostData } from '../../lib/posts';
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
 
 import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    return {
-      props: {
-        postData,
-      },
-    };
-  }
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
+
+
+export default function Post() {
+
+  const router = useRouter()
+  const { id } = router.query
+  const [postData, setPostData] = useState({})
+
+  useEffect(() => {
+    if(!id) return
+    async function fetchPosts() {
+      await fetch('/api/posts/' + id, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((response) => {
+        if (response.status === 200) {
+          const data = response.json().then((data) => {
+            setPostData(data)
+          });
+        } 
+      })
+    }
+    fetchPosts()
+
+  }, [id, router])
+
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
-
-export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-      </Layout>
-    );
-  }
