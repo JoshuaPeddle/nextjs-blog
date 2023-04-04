83f22a9f06f5fa946abae1aecf421d9f75366f0e
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-04-04 01:32:44 -0230

    Convert index to ssr

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,37 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
+import clientPromise from '../lib/mongodb'
+
+
+
+export async function getServerSideProps(context) {
+  const allPostsData =  getSortedPostsData(); // TODO: add error handling
+
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true, 
+        allPostsData, },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false,
+        allPostsData, },
+    }
+  }
 }
+
 
 export default function Home({ allPostsData }) {
   return (
