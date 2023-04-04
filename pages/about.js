226b286f3a125687f48278049e605c4ebe86d9bd
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-04-04 14:45:45 -0230

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
         <Layout about>
             <h1>About</h1>
             <p>
-                I'm a software engineer from the United States. I'm currently working at <Link href="https://www.uber.com">Uber</Link> as a software engineer. I'm also a <Link href="https://www.github.com/joshuapeddle">Github</Link> contributor.
+                I'm a software developer from Canada. I studied computer science at Memorial University. I'm also a <Link href="https://www.github.com/joshuapeddle">Github</Link> contributor.
             </p>
         </Layout>
 
