13e0d19edcb934a60f938d85d2aa8e068fa44a18
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-04-04 12:58:37 -0230

    Get posts from server at runtime

--- pages/editor.js
+++ pages/editor.js
@@ -1,36 +1,17 @@
 import MDEeditor from "../components/editor";
+import {getSortedPostsData} from '../lib/posts-db'
 
+
+export async function getServerSideProps(context) {
+    const allPostsData = await getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
 export default function Editor() {
     return (
         <MDEeditor />  
     );
 }
-
-
-/*import ReactDOMServer from "react-dom/server";
-
-export const CustomPreview = () => {
-  const customRendererOptions = useMemo(() => {
-    return {
-      previewRender() {
-        return ReactDOMServer.renderToString(
-          <ReactMarkdown
-            source={text}
-            renderers={{
-              CodeBlock: CodeRenderer,
-              Code: CodeRenderer,
-            }}
-          />
-        );
-      },
-    } as SimpleMDE.Options;
-  }, []);
-
-  return (
-    <div>
-      <h4>Custom preview</h4>
-      <SimpleMdeReact options={customRendererOptions} />
-    </div>
-  );
-};
-*/
