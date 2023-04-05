cee7f8871318f5909444b80bfb80f0f858c2adec
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-04-04 21:33:41 -0230

    Update cypress URL

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://192.168.2.162:3000',
+    baseUrl: 'http://localhost:3000',
   },
   component: {
     devServer: {
