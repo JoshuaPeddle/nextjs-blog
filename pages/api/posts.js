78297e1dc53c037c6f5639b9ce5b2bd01f148275
JoshuaPeddle <joshuapeddle128@gmail.com>
2023-04-05 23:22:22 -0230

    Send the new post id to the client

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -5,8 +5,8 @@ export default async function handler(req, res) {
     // Process a POST request
     const { id, title, content } = req.body;
     try {
-      await savePost(id, title, content);
-      res.status(200).json({ success: true });
+      const newId = await savePost(id, title, content);
+      res.status(200).json({ success: true, id: newId });
     } catch (e) {
       res.status(500).json({ error: e.message });
     }
